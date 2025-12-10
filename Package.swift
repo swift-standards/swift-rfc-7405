8eb81f7133d28be6716f672a3127a2499feb7b39
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:16:52 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ import PackageDescription
 let package = Package(
     name: "swift-rfc-7405",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(
