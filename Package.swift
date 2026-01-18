27811df70ef70d6145baf2e356c3fc81cf702dc2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 10:53:36 +0100

    Switch package dependencies from remote URLs to local paths

--- Package.swift
+++ Package.swift
@@ -26,10 +26,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", from: "0.0.1"),
-        .package(url: "https://github.com/swift-primitives/swift-test-primitives.git", from: "0.0.1"),
-        .package(url: "https://github.com/swift-foundations/swift-ascii.git", from: "0.0.1"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5234.git", from: "0.0.1")
+        .package(path: "../../swift-primitives/swift-standard-library-extensions"),
+        .package(path: "../../swift-primitives/swift-test-primitives"),
+        .package(path: "../../swift-foundations/swift-ascii"),
+        .package(path: "../swift-rfc-5234")
     ],
     targets: [
         .target(
